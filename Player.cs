e3e134d56a42e97cad8af00de6b20a0987ac2856
JoseLucas1303 <74744598+JoseLucas1303@users.noreply.github.com>
2022-08-04 13:43:31 -0300

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Media;
+using WMPLib;
+
+namespace ArticPolar.Dev.BackgroundMusic
+{
+    public class Player
+    {
+        public static void WavPlayer(string WavPath, bool PlayLooping, int LoadTimeout)
+        {
+            SoundPlayer Wavplayer = new SoundPlayer();
+            Wavplayer.SoundLocation = WavPath;
+            if (PlayLooping == true) { Wavplayer.PlayLooping(); }
+            Wavplayer.LoadTimeout = LoadTimeout;
+            Wavplayer.Play();
+        }
+
+        public static void Mp3Player(string Mp3Path)
+        {
+            WindowsMediaPlayer wplayer = new WindowsMediaPlayer();
+            wplayer.URL = Mp3Path;
+            wplayer.controls.play();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ArticPolar.Dev.BackgroundMusic")]
+[assembly: AssemblyDescription("Play Wav/Mp3 File on C# WinForm or C# Console in Background")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ArticPolar Apps")]
+[assembly: AssemblyProduct("ArticPolar.Dev.BackgroundMusic")]
+[assembly: AssemblyCopyright("Copyright ©  ArticPolar Apps 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3f2b3a0b-6038-42c4-b218-c989b2493d87")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
